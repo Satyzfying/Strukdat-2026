453294312cda9168041205905ba90b50ff7df4cc
Satyzfying <satyaputraaryanta@gmail.com>
2026-03-04 18:32:05 +0700

    Test

---
+++ Project Strukdat OOP/src/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+    
+    String mood;
+    int energy;
+    int hunger;
+
+    
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
